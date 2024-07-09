8f3a90af96ded3d8e9e5266eb69b7ca6a046b61e
Priyadharsheni K.T <priyad8322@gmail.com>
2024-07-09 11:36:11 +0530

    Update home.java

--- home.java
+++ home.java
@@ -3,183 +3,83 @@ package airline;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.sql.*;
+import java.net.URL;
 
+class test1 implements ActionListener {
+    JFrame frame1 = null;
+    JPanel panel;
+    JButton b1, b2 = null;
+    JLabel l1, l2, l3, l4;
 
-class test9 implements ActionListener
-{
-    
-    private String origin;
-    private String dest;
-    private String date;
-    private String time;
-    private String c;
-    
-    JFrame f=null;
-    JTextArea ta=null;
-    JLabel l1;
-    JButton b1=null;
-    Font font;
-    
-    test9(String o,String d,String da,String t,String c)
-    {
-        this.origin=o;
-        this.dest=d;
-        this.date=da;
-        this.time=t;
-        this.c=c;
-       
-        f=new JFrame("Bill");
-         
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setBackground(new Color(176, 224, 230));
-        
-        l1=new JLabel("TICKET PAYMENT");
-        l1.setFont(new Font("Arial",Font.BOLD,32));
-        ta=new JTextArea();
-        b1=new JButton("Pay");
-        
-        
-        l1.setBounds(650,80,500,100);
-        ta.setBounds(550,200,500,250);
-        b1.setBounds(750,550,100,50);
-        
-        font=new Font("Arial",Font.BOLD,17);
-        ta.setFont(font);
-        
-        
-        f.add(l1);
-        f.add(ta);
-        f.add(b1);
-        
+    test1(String u, String p) {
+        frame1 = new JFrame("Home page");
+
+        frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        panel = new JPanel();
+        panel.setBackground(new Color(173, 216, 235));
+        panel.setLayout(null);
+
+        l1 = new JLabel("Welcome to PT Airlines");
+        l1.setFont(new Font("Arial", Font.BOLD, 45));
+        l1.setBounds(530, 60, 1000, 70);
+
+        l2 = new JLabel("We are leading airlines in India since 2003.");
+        l2.setFont(new Font("Arial", Font.BOLD, 20));
+        l2.setBounds(580, 180, 1000, 50);
+
+        panel.add(l2);
+
+        l3 = new JLabel("We happily welcome you to our family. Please enjoy your journey with us!!");
+        l3.setFont(new Font("Arial", Font.BOLD, 20));
+        l3.setBounds(450, 220, 1000, 50);
+
+        panel.add(l3);
+
+        try {
+            URL imageUrl = new URL("https://images.cnbctv18.com/wp-content/uploads/2023/11/air-india.jpg"); 
+            ImageIcon i = new ImageIcon(imageUrl);
+            l4 = new JLabel(i);
+            l4.setBounds(240, 330, 1105, 370);
+            panel.add(l4);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        b1 = new JButton("Login");
+        b2 = new JButton("Register");
+
+        panel.add(l1);
+        panel.add(b1);
+        panel.add(b2);
+
+        b1.setBounds(460, 740, 150, 38);
+        b2.setBounds(1050, 740, 150, 38);
+
         b1.addActionListener(this);
-        
-        
-        f.setExtendedState(JFrame.MAXIMIZED_BOTH); 
-        f.setLayout(null);
-        f.setSize(1000,700);
-        f.setVisible(true);
-        
-        String url = "jdbc:mysql://localhost:3306/intern";
-        String user = "root";
-        String pass = "pt2029";
-        String sql = "SELECT Cost FROM flight WHERE Origin=? AND Destination=? AND Timing=? AND Class=?;";
+        b2.addActionListener(this);
 
-        try 
-        {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            try (Connection con = DriverManager.getConnection(url,user,pass); 
-                    PreparedStatement statement = con.prepareStatement(sql)) 
-            {
-                statement.setString(1, origin);
-                statement.setString(2, dest);
-                statement.setString(3, time);
-                statement.setString(4, c);
-                try (ResultSet res = statement.executeQuery()) 
-                {
-                    float cost;
-                    if(res.next())
-                    {
-                       cost= res.getFloat("Cost");
-                       if(time.equals("EM"))
-                           time="Early Morning(1:00-6:00am)";
-                       else if(time.equals("M"))
-                           time="Morning(7:00-10:00am)";
-                       else if(time.equals("A"))
-                           time="Afternoon(12:00-3:00pm)";
-                       else if(time.equals("E"))
-                           time="Evening(5:00-8:00pm)";
-                       else
-                            time="Late Evening(9:00-11:00pm)";
-                        
-                        String t1="Origin: "+origin;
-                        String t2="Destination: "+dest;
-                        String t3="Date: "+date;
-                        String t4="Time: "+time;
-                        String t5="Class: "+c;
-                        String t6="Total Cost: "+cost+" /- Rs ";
-                        ta.setText(t1+"\n\n"+t2+"\n\n"+t3+"\n\n"+t4+"\n\n"+t5+"\n\n"+t6);
-                        
-                    }
-                }
-            }
-        } 
-        catch (ClassNotFoundException e) 
-        {
-        } 
-        catch (SQLException e) 
-        {
-            JOptionPane.showMessageDialog(f, "Payment failed", "Error", JOptionPane.ERROR_MESSAGE);
-        }       
+        frame1.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame1.getContentPane().add(panel);
+        frame1.setSize(400, 750);
+        frame1.setVisible(true);
     }
-    
-    
+
     @Override
-    public void actionPerformed(ActionEvent ae)
-    {
-        if(ae.getActionCommand().equals("Pay"))
-        {
-            int option = JOptionPane.showConfirmDialog(f, "Click yes to proceed", "Confirmation", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
-            if (option == JOptionPane.YES_OPTION) 
-            {
-                int op = JOptionPane.showOptionDialog(null, "Payment successful", "Payment confirmation", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, null, null);
-                if (op == JOptionPane.OK_OPTION) 
-                {
-                    test5 t5=new test5();
-                    f.setVisible(false);
-                    String url = "jdbc:mysql://localhost:3306/intern";
-                    String user = "root";
-                    String pass = "pt2029";
-                    try
-                    {
-                    Class.forName("com.mysql.cj.jdbc.Driver");
-                    Connection con=DriverManager.getConnection(url,user,pass);  
-                    String sql1="UPDATE flight SET Seats = Seats - 1 WHERE Origin =? AND Destination =? AND Timing =? AND Class=?;";                  
-                    PreparedStatement statement1 = con.prepareStatement(sql1);
-                    statement1.setString(1, origin);
-                    statement1.setString(2, dest);
-                             if (time.equals("Early Morning(1:00-6:00am)")) 
-                              time = "EM";
-                             else if (time.equals("Morning(7:00-10:00am)"))                              
-                              time = "M";                               
-                             else if (time.equals("Afternoon(12:00-3:00pm)"))                             
-                              time = "A";                              
-                             else if (time.equals("Evening(5:00-8:00pm)"))                              
-                              time = "E";                            
-                             else                             
-                              time = "LE";
-                             
-            
-                    statement1.setString(3,time);
-                    statement1.setString(4, c);
-                    statement1.executeUpdate();                   
-                    }
-                    catch (ClassNotFoundException e) 
-                   {
-                   } 
-                    catch (SQLException e) 
-                    {
-                   JOptionPane.showMessageDialog(f, "Payment failed", "Error", JOptionPane.ERROR_MESSAGE);
-                    }                                         
-                }
-            }
-            else
-            {
-               test6 t6=new test6();//back to search
-                
-            }
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getActionCommand().equals("Login")) {
+            test2 t2 = new test2();
+        } else if (ae.getActionCommand().equals("Register")) {
+            test3 t3 = new test3();
         }
     }
+
 }
-    
 
-
-
-public class bill 
-{
-    public static void main(String ar[])
-    {
-        
+public class home {
+    public static void main(String ar[]) {
+        test2 t = new test2();
+        t.f.setVisible(false);
+        test1 t1 = new test1(test2.username, test2.password);
     }
-    
 }
